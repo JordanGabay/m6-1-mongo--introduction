33724287da86dba145f2c475c2ed441fef7d77b1
Jordan Gabay <gabay.jordan@outlook.com>
2020-08-17 16:03:06 -0400

    Exercise 1.2 Complete

---
+++ exercises/exercise-1.2.js
@@ -0,0 +1,23 @@
+const {MongoClient} = require('mongodb');
+
+require('dotenv').config();
+const {MONGO_URI} = process.env;
+
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}
+
+async function getCollection(database) {
+    const client = await MongoClient(MONGO_URI, {useUnifiedTopology: true})
+
+    await client.connect();
+
+    const db = client.db(database);
+    // console.log("connected!")
+    const data = await db.collection('users').find().toArray();
+    // console.log(data)
+    client.close();
+}
+
+getCollection('exercise_1')
