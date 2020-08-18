e87e82bdc7e32f691fba84c2ff7dc8d045eaa88d
Jordan Gabay <gabay.jordan@outlook.com>
2020-08-18 15:59:18 -0400

    Exercise 2.5 Complete

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const { getUsers } = require("./exercises/exercise-1.3");
 const { addUser } = require("./exercises/exercise-1.4");
-const {createGreeting, getGreeting, getGreetings} = require('./exercises/exercise-2')
+const {createGreeting, getGreeting, getGreetings, deleteGreeting} = require('./exercises/exercise-2')
 
 const PORT = process.env.PORT || 8000;
 
@@ -24,6 +24,7 @@ express()
   .post('/exercise-2/greeting', createGreeting)
   .get('/exercise-2/greeting/:_id', getGreeting)
   .get('/exercise-2/greetings', getGreetings)
+  .delete('/exercise-2/greeting/:_id', deleteGreeting)
   .use((req, res) => res.status(404).type("txt").send("🤷‍♂️"))
 
   .listen(PORT, () => console.log(`Listening on port ${PORT}`));
